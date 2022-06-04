40f284da665154b209c22084498974e573a32fbc
eziz2201 <e_myradow@mail.ru>
2022-06-04 05:52:21 +0300

    feat(app): add books-slider

---
+++ src/components/BooksSlider/styles.ts
@@ -0,0 +1,10 @@
+import Slider from "react-slick";
+import styled from "styled-components";
+
+const StyledSlider = styled(Slider)``;
+
+const StyledBook = styled.div`
+  padding: 10px 15px;
+`;
+
+export { StyledSlider, StyledBook };
