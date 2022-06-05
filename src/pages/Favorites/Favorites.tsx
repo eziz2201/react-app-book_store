4ab7eb3310db25eb213a5508f6d9c4ca9b65a257
eziz2201 <e_myradow@mail.ru>
2022-06-05 11:58:25 +0300

    fix(app): fix app

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,13 +1,16 @@
 import { useNavigate } from "react-router-dom";
+import BooksSlider from "../../components/BooksSlider/BooksSlider";
 import FavoriteBook from "../../components/FavoriteBook/FavoriteBook";
 import IconSelector from "../../components/IconSelector/IconSelector";
 import Subscribe from "../../components/Subscribe/Subscribe";
 import { useAppSelector } from "../../store/hooks/hooks";
 import { getFavoriteBooks } from "../../store/selectors/favoriteBooksSelectors";
+import { getRecommendedBooks } from "../../store/selectors/recommendedBooksSelector";
 import { IFavoriteBook } from "../../types/types";
 import {
   StyledBackButton,
   StyledFavorites,
+  StyledSliderTitle,
   StyledText,
   StyledTitle,
 } from "./styles";
@@ -15,6 +18,7 @@ import {
 const Favorites = () => {
   const navigate = useNavigate();
   const favoriteBooks: IFavoriteBook[] = useAppSelector(getFavoriteBooks);
+  const recommendedBooks = useAppSelector(getRecommendedBooks);
 
   const handleBack = () => {
     navigate(-1);
@@ -32,7 +36,8 @@ const Favorites = () => {
           <FavoriteBook key={book.isbn13} book={book} />
         ))
       )}
-      <Subscribe />
+      <StyledSliderTitle>Recommended Books</StyledSliderTitle>
+      <BooksSlider books={recommendedBooks} />
     </StyledFavorites>
   );
 };
